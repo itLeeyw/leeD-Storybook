1149efc83415f98e1f6779473a55f2022c657064
itleeyw <itleeyw@gmail.com>
2020-09-21 22:43:28 +0800

    .

---
+++ precache-manifest.8a2971598c9bdfaf13aedc82aeb37e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d6dbfd842533f49b245c894db98d76",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e5372521c1040f2d5841.bundle.js"
+  },
+  {
+    "url": "runtime~main.e5372521c1040f2d5841.bundle.js"
+  },
+  {
+    "revision": "da6ef68f0676ac61318a",
+    "url": "static/css/main.0d4e2447.chunk.css"
+  },
+  {
+    "url": "vendors~main.e5372521c1040f2d5841.bundle.js"
+  },
+  {
+    "url": "vendors~main.e5372521c1040f2d5841.bundle.js.LICENSE.txt"
+  }
+]);
